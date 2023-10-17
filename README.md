96c468382e7a2d1eea6447219fdcd13b32e214cb
Charon-Fan <Charon.Dian@gmail.com>
2023-10-17 16:36:35 +0800

    docs: indicate project deprecation

---
+++ README.md
@@ -0,0 +1,5 @@
+# eth-keystone-bridge-keyring
+
+## warning DEPRECATION NOTICE
+
+This repository is deprecated and is no longer maintained or updated.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello world');
+window.addEventListener('message', function(event) {
+  if (event.data?.target === 'keystone3') {
+    console.log('Received message:', event.data);
+
+    const encodedData = btoa(JSON.stringify(event.data));
+
+    event.source?.postMessage(encodedData, event.origin as any);
+  }
+});
